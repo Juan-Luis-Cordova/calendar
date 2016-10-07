3cabe09867ded43fa289c60b5f6a2a0900601f41
Juan-Luis-Cordova <juanluis0270@gmail.com>
2016-10-06 21:55:59 -0300

    Add files via upload

--- version.php
+++ version.php
@@ -29,5 +29,5 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin->pluginname = "local_calendar";
 $plugin->component = 'local_calendar';
-$plugin->version   = '2016092701';
+$plugin->version   = '2016100601';
 ?>
